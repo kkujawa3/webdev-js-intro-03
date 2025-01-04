ec7f9edc931497b14ceea2bd5ee3ee71528459bd
unknown <kimberllee.al@gmail.com>
2025-01-04 15:38:30 -0500

    Completed JS assignments part 1 and 2.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,30 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+console.log(13 % 5);          // output: 3
+console.log(2 % 3);           // output: 2
 
+console.log((10 * 2) + 4);    // output: 24 multiply first
+console.log(2 * (3 + 10));    // output: 26 add then multiply
+
+console.log( 5 == 5);         // output: true
+
+console.log((3 * 4) / 3 + 2); // output: 6 multiply, divide then add
+
+console.log( 3 * 4 + 2);      // output: 14 multiply then add
+
+console.log( 1 < 2 && 2 < 3); // output: true
+console.log(6 < 4 && 9 < 3);  // output: false
+
+console.log(10 !== "10");     // output: true
+console.log(9 == 10);         // output: false
+
+console.log(2 >= 3);         // output: false
+console.log(8 >= 4 + 4);     // output: true
+
+console.log(3 || 2);         // output: 3
+
+console.log(18 % 4);         // output: 2
+console.log(8 % 3);          // output: 2
+*/
